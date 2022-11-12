900239983797ab9341f366c370c822a66e04e708
TheTrueShaman <68354378+TheTrueShaman@users.noreply.github.com>
2022-11-11 20:59:27 -0800

    Update game.java

--- game.java
+++ game.java
@@ -31,13 +31,13 @@ public class game {
         while (i < 1000) {
             if (settings.gameState == 1) {
                 alive = update(alive);
-                display.draw_life(alive, settings);
+                //display.draw_life(alive, settings);
             }
             i++;
         }
         long stop = System.currentTimeMillis();
         System.out.println(stop - start);
-        //System.exit(0);
+        System.exit(0);
     }
 
     static void run(display display, settings settings) {
@@ -56,13 +56,65 @@ public class game {
         }
     }
 
-    static int check(int[][] arr, int[] toCheckValue) {
-        // This is inefficient. Implement quick sort and binary search.
-        for (int i = 0; i < arr.length; i++) {
-            if (Arrays.equals(arr[i], toCheckValue)) {
+    // Code for quicksort taken from stack abuse and modified.
+    static void quicksort(int[][] arr, int low, int high){
+        if (low < high){
+            int p = partition(arr, low, high);
+            quicksort(arr, low, p-1);
+            quicksort(arr, p+1, high);
+        }
+    }
+
+    static int partition(int[][] arr, int low, int high){
+        int p = low, j;
+        for (j=low+1; j <= high; j++) {
+            if (arr[j][0] < arr[low][0]) {
+                swap(arr, ++p, j);
+            } if (arr[j][0] == arr[low][0] && arr[j][1] < arr[low][1]) {
+                swap(arr, ++p, j);
+            }
+        }
+
+        swap(arr, low, p);
+        return p;
+    }
+
+    static void swap(int[][] arr, int low, int pivot){
+        int[] tmp = arr[low];
+        arr[low] = arr[pivot];
+        arr[pivot] = tmp;
+    }
+
+    // Code for binary search taken from stack abuse and modified.
+    static int search(int[][] arrayToSearch, int[] element) {
+        int lowIndex = 0;
+        int highIndex = arrayToSearch.length-1;
+
+        while (lowIndex <= highIndex - 64) {
+            int midIndex = (lowIndex + highIndex) / 2;
+            if (element[0] == arrayToSearch[midIndex][0]) {
+                if (element[1] == arrayToSearch[midIndex][1]) {
+                    return midIndex;
+                } else if (element[1] < arrayToSearch[midIndex][1]) {
+                    highIndex = midIndex-1;
+                } else if (element[1] > arrayToSearch[midIndex][1]) {
+                    lowIndex = midIndex+1;
+                }
+            } else if (element[0] < arrayToSearch[midIndex][0]) {
+                highIndex = midIndex-1;
+            } else if (element[0] > arrayToSearch[midIndex][0]) {
+                lowIndex = midIndex+1;
+            }
+        }
+        // Use linear search when the entire section to search can fit in the cache.
+        // I'd assume 64 elements can fit in the cache, more probably can, but I don't feel like testing.
+        // Also, what I'm saying might actually be total BS, I'm no expert.
+        for (int i = lowIndex; i < highIndex + 1; i++) {
+            if (Arrays.equals(arrayToSearch[i], element)) {
                 return i;
             }
         }
+        // Returns -1 if the value is not present in the array.
         return -1;
     }
 
@@ -80,7 +132,7 @@ public class game {
     static int count_alive_neighbors(int[][] alive, int[] cell) {
         int alive_num = 0;
         for (int i = 0; i < 8; i++) {
-            if (check(alive, neighbor_cell(cell, i)) != -1) {
+            if (search(alive, neighbor_cell(cell, i)) != -1) {
                 alive_num++;
             }
         }
@@ -88,15 +140,16 @@ public class game {
     }
 
     static int[][] update(int[][] alive) {
+        quicksort(alive, 0, alive.length - 1);
         int[][] deadNearAlive = {};
         for (int[] cell : alive) {
             for (int i = 0; i < 8; i++) {
                 int[] neighbor = neighbor_cell(cell, i);
-                int location = check(alive, neighbor);
+                int location = search(alive, neighbor);
                 if (location != -1) {
                     continue;
                 }
-                location = check(deadNearAlive, neighbor);
+                location = search(deadNearAlive, neighbor);
                 if (location == -1) {
                     deadNearAlive = append(deadNearAlive, neighbor);
                 }
@@ -111,6 +164,7 @@ public class game {
             }
         }
 
+        quicksort(deadNearAlive, 0, deadNearAlive.length - 1);
         for (int[] cell : deadNearAlive) {
             int alive_num = count_alive_neighbors(alive, cell);
             if (alive_num == 3) {
